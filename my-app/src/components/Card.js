258cf97ef571ba4badaeaffdbe289a4f03131973
Daryl Estrada <frdarylestrada@gmail.com>
2022-11-18 15:01:02 +0700

    Write Stories

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,34 +1,62 @@
 import React from "react";
 import './Card.css';
+import PropTypes, { func } from "prop-types"
 
-function Card({title, imageUrl, body}){
-    return(
-        
-        <div className="card-container">
-            
-            <div className="image-container">
-                <img src = {imageUrl} alt=''/>
-            </div>
+function Card({ title, imageUrl, body, onClick, children, spacing = 2, direction = "row", wrap = false }) {
+    const style = {
+        display: "flex",
+        gap: `${spacing * 0.25}rem`,
+        flexWrap: wrap ? "wrap" : "nowrap",
+        flexDirection: direction,
+    }
+    return (
 
-            <div className="card-content">
-                <div className="card-title">
-                    <h3>{title}</h3>
+        <div style={style}>
+
+            {children}
+
+       {/*  <div className="card-container">
+
+                <div className="image-container">
+                    <img src={imageUrl} alt='' />
                 </div>
 
-                <div className="card-body">
-                    <p>{body}</p>
-                </div>
-            </div>
+                <div className="card-content">
+                    <div className="card-title">
+                        <h3>{title}</h3>
+                    </div>
 
-            <div className="btn">
-                <button>
-                    <a>
-                        View More
-                    </a>
-                </button>
-            </div>
+                    <div className="card-body">
+                        <p>{body}</p>
+                    </div>
+                </div>
+
+                <div className="btn">
+                    <button onClick={onClick}>
+                        <a>
+                            View More
+                        </a>
+                    </button>
+                </div>
+    </div>*/} 
+
         </div>
+
+
     )
+
 }
+
+Card.propTypes = {
+    onClick: PropTypes.func,
+    spacing: PropTypes.number,
+    wrap: PropTypes.bool,
+    direction: PropTypes.oneOf(["row", "column"]),
+}
+
+Card.defaultProps = {
+    onClick: undefined,
+};
+
 
 export default Card
--- my-app/src/stories/Card.stories.js
+++ my-app/src/stories/Card.stories.js
@@ -3,16 +3,56 @@ import '../components/Card.css'
 
 export default {
 
-  title: 'Example/Card',
+  title: 'Components/Card',
   component: Card,
+  argTypes: {
+    numberOfChildren: { type: "number", defaultValue: 4 }
+  }
 
 };
 
-const Template = args => <Card {...args} />
+const Template = ({ numberOfChildren, ...args }) =>
+  <Card {...args}>
+
+    {[...Array(numberOfChildren).keys()].map(n => (
+      <Card>
+        <div className="card-container">
+
+          <div className="image-container">
+            <img src='https://media.tenor.com/NICoVNbKVGYAAAAC/profile-picture.gif' alt='' />
+          </div>
+
+          <div className="card-content">
+            <div className="card-title">
+              <h3>Lorem ipsum</h3>
+            </div>
+
+            <div className="card-body">
+              <p>'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.',</p>
+            </div>
+          </div>
+
+          <div className="btn">
+            <button>
+              <a>
+                View More
+              </a>
+            </button>
+          </div>
+        </div>
+
+      </Card>
+    ))}
+
+  </Card>
 
 export const CardLoremIpsum = Template.bind({})
 CardLoremIpsum.args = {
   title: 'Lorem ipsum',
   imageUrl: 'https://media.tenor.com/NICoVNbKVGYAAAAC/profile-picture.gif',
-  body: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.'
+  body: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.',
+  direction: "row",
+  spacing: 2,
+  wrap: false,
 }
+CardLoremIpsum.storyName = 'Card Lorem Ipsum'
