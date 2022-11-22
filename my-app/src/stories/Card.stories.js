9d87c4269c43286d370615fe370752b840ea1df6
Daryl Estrada <frdarylestrada@gmail.com>
2022-11-22 14:59:55 +0700

    Initial Commit

--- my-app/src/stories/Card.stories.js
+++ my-app/src/stories/Card.stories.js
@@ -28,7 +28,7 @@ const Template = ({ numberOfChildren, ...args }) =>
             </div>
 
             <div className="card-body">
-              <p>'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.',</p>
+              <p>OKAY</p>
             </div>
           </div>
 
